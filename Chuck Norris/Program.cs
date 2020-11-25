07183a757f0e2d88b641fdbb14cdb57922455dd3
Michael Enkner <michael.enkner6@gmail.com>
2020-11-25 16:09:18 +0100

    started

---
+++ Chuck Norris/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Chuck_Norris
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
